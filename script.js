c59c25a1c1e14a9c74c61434987227363dda4384
saumelliv <saumelliv@gmail.com>
2020-05-05 19:36:09 -0400

    El proyecto esta terminado

---
+++ script.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#welcome',{
+     duration: 2000,
+     origin: 'left',
+     distance: '300px'
+});
+sr.reveal('#im',{
+     duration: 2000,
+     origin: 'right',
+     delay: 1000
+});
+sr.reveal('#coding',{
+     duration: 2000,
+     origin: 'right',
+     distance: '300px',
+     viewFactor: 0.2
+});
+sr.reveal('#team',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('#titlepro',{
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('#pro',{
+    duration: 2000,
+    origin: 'right',
+    delay: 1000
+});
+sr.reveal('#expe',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
